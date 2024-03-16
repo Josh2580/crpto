bc0836a8537fa781da2f24bc5edbde9957f70f9b
Joshua <joshuaugwuede@gmail.com>
2024-03-16 16:17:13 +0100

    home

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,16 +9,19 @@ import {
   FaWhatsapp,
 } from "react-icons/fa";
 
+import LogoImg from "../assets/CDMLogo.png";
+
 const Profile = () => {
   return (
     <>
-      {/* <div className=" d-flex align-items-center text-light justify-content-between px-4 fs-6 rounded-4">
-        <p className="m-0 px-2 py-0 border border-secondary  rounded-5">
-          Connect Wallet
-        </p>
-        <p className="m-0 p-1">N</p>
-      </div> */}
-      <div className="d-flex text-light flex-column  align-items-center rounded-4 mx-auto w-100 p-5 mt-5 gap-3 border border-secondary ">
+      <div className=" d-flex ">
+        <img
+          src={LogoImg}
+          style={{ height: "70px", margin: "0px" }}
+          alt="CDM Logo"
+        />
+      </div>
+      <div className="d-flex m-0 text-light flex-column  align-items-center rounded-4 w-100 p-5 gap-3 border border-secondary ">
         <div className="d-flex flex-column align-items-center">
           <p className="p-0 m-0 fs-6">Earned CDM</p>
           <h3>$CDM 0.00</h3>

c66d882ef5e1f3b81c98b9299f203a60e5d8c5e0
Joshua <joshuaugwuede@gmail.com>
2024-04-03 07:59:32 +0100

    tele

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,10 +3,12 @@ import { Container, Col, Row } from "react-bootstrap";
 import Gain from "../components/Gain";
 import Dollar from "../components/Dollar";
 import Profile from "../components/Profile";
+import { useTelegramWebhookQuery } from "../api/paymentApi";
 
 function Homepage() {
-  // isSuccess && console.log(data);
-  // error && console.log(error);
+  const { data, error, isLoading, isSuccess } = useTelegramWebhookQuery();
+  isSuccess && console.log(data);
+  error && console.log(error);
 
   return (
     <div className="d-flex flex-column bg-dark gap-4">
